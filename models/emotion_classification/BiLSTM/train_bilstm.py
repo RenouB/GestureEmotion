changed weight saving procedure, fixed softmax error on att weights

models/emotion_classification/BiLSTM/train_bilstm.py
@@ -139,10 +139,9 @@ if __name__ == '__main__':
 	input_dim = get_input_dim(args.keypoints, args.input)
 	scores_per_fold = {'train':{}, 'dev':{}}
 
-	best_f1 = 0
 
 	for k in range(args.num_folds):
-
+		best_f1 = 0
 		logger.new_fold(k)
 		torch.manual.seed(200)
 		if not args.joint:
@@ -231,7 +230,7 @@ if __name__ == '__main__':
 				best_f1 = f1
 				best_epoch = epoch
 				best_fold = k
-				torch.save(model.state_dict(), os.path.join(write_dir, 'weights', basename+'.weights'))
+				torch.save(model.state_dict(), os.path.join(write_dir, 'weights', basename+'fold{}'.format(k)+'.weights'))
 
 
 

models/emotion_classification/JointBiLSTM/joint_bilstm.py
@@ -19,8 +19,8 @@ class JointBiLSTM(nn.Module):
         self.evidenceB = nn.Sequential(nn.Linear(hidden_dim*2, attention_dim), nn.ReLU(), nn.Dropout())
         self.attention_vector = nn.Sequential(nn.Linear(attention_dim, 1), nn.ReLU(), nn.Dropout())
         self.classify = nn.Linear(attention_dim, 1)
-        self.softmax = nn.Softmax(dim=0)
-        # print(self)
+        self.softmax = nn.Softmax(dim=1)
+
     def forward(self, posesA, posesB):
 
         A = torch.transpose(posesA, dim0=1, dim1=0)

models/emotion_classification/JointBiLSTM/train_joint_bilstm.py
@@ -153,9 +153,9 @@ if __name__ == '__main__':
 	input_dim = get_input_dim(args.keypoints, args.input)
 	scores_per_fold = {'train':{}, 'dev':{}}
 
-	best_f1 = 0
 
 	for k in range(args.num_folds):
+		best_f1 = 0
 
 		logger.new_fold(k)
 
@@ -246,7 +246,7 @@ if __name__ == '__main__':
 				best_f1 = f1
 				best_epoch = epoch
 				best_fold = k
-				torch.save(model.state_dict(), os.path.join(write_dir, 'weights', basename+'.weights'))
+				torch.save(model.state_dict(), os.path.join(write_dir, 'weights', basename+'fold{}'.format(fold)+'.weights'))
 
 
 
@@ -269,7 +269,7 @@ if __name__ == '__main__':
 			class_one_scores[1], class_one_scores[2]))
 		f.write("{:8} {:8.4f} {:8.4f} {:8.4f} {:8.4f} \n".format("macro",
 				macro_scores[0], macro_scores[1], macro_scores[2], av_scores['dev']['acc'][best_epoch][0]))
-				
+
 
 	logger.close(0, 0)
 	print("STARTIME {}".format(starttime))

models/emotion_classification/Linear/train_linear.py
@@ -113,9 +113,9 @@ if __name__ == '__main__':
 	input_dim = 80
 	scores_per_fold = {'train':{}, 'dev':{}}
 
-	best_f1 = 0
 
 	for k in range(args.num_folds):
+		best_f1 = 0
 
 		logger.new_fold(k)
 
@@ -205,9 +205,7 @@ if __name__ == '__main__':
 				best_f1 = f1
 				best_epoch = epoch
 				best_fold = k
-				torch.save(model.state_dict(), os.path.join(write_dir, 'weights', basename+'.weights'))
-
-
+				torch.save(model.state_dict(), os.path.join(write_dir, 'weights', basename+'fold{}'.format(k)+'.weights'))
 
 	av_scores = average_scores_across_folds(scores_per_fold)
 	scores = {'av_scores':av_scores, 'all':scores_per_fold}

models/emotion_classification/MultiChannelCNN/base_models.py
@@ -42,14 +42,14 @@ class OneActorOneModalityDeltas(nn.Module):
 		self.cnn_block3 = nn.Sequential(CNN(feats_dim, n_filters, filter_sizes, cnn_output_dim, dropout),
 						nn.ReLU())
 
-		self.project1 = nn.Sequential(nn.Linear(cnn_output_dim, project_output_dim), nn.Dropout(), nn.ReLU())
-		self.project2 = nn.Sequential(nn.Linear(cnn_output_dim, project_output_dim), nn.Dropout(), nn.ReLU())
-		self.project3 = nn.Sequential(nn.Linear(cnn_output_dim, project_output_dim), nn.Dropout(), nn.ReLU())
-
-		self.score1 = Attention(project_output_dim, att_vector_dim, dropout)
-		self.score2 = Attention(project_output_dim, att_vector_dim, dropout)
-		self.score3 = Attention(project_output_dim, att_vector_dim, dropout)
-
+		self.project1 = nn.Sequential(nn.Linear(cnn_output_dim, project_output_dim), nn.ReLU(), nn.Dropout())
+		self.project2 = nn.Sequential(nn.Linear(cnn_output_dim, project_output_dim), nn.ReLU(), nn.Dropout())
+		self.project3 = nn.Sequential(nn.Linear(cnn_output_dim, project_output_dim), nn.ReLU(), nn.Dropout())
+		self.evidenceA = nn.Sequential(nn.Linear(project_output__dim, attention_dim), nn.ReLU(), nn.Dropout())
+		self.evidenceB = nn.Sequential(nn.Linear(project_output__dim, attention_dim), nn.ReLU(), nn.Dropout())
+		self.evidenceC = nn.Sequential(nn.Linear(project_output__dim, attention_dim), nn.ReLU(), nn.Dropout())
+		self.attention_vector = nn.Sequential(nn.Linear(attention_dim, 1), nn.ReLU(), nn.Dropout())
+		self.softmax = nn.Softmax(dim=1)
 		self.classify = nn.Linear(project_output_dim, num_classes)
 
 	def forward(self, poses, deltas, delta_deltas):
@@ -58,21 +58,23 @@ class OneActorOneModalityDeltas(nn.Module):
 
 		poses = self.cnn_block1(poses)
 		poses = self.project1(poses)
-		score1 = self.score1(poses)
+		evidence1 = self.evidence1(poses)
+		score1 = self.attention_vector(evidence1)
 
 		deltas = self.cnn_block2(deltas)
 		deltas = self.project2(deltas)
-		score2 = self.score2(deltas)
+		evidence2 = self.evidence2(deltas)
+		score2 = self.attention_vector(evidence2)
 
 		delta_deltas = self.cnn_block3(delta_deltas)
 		delta_deltas = self.project3(delta_deltas)
-		score3 = self.score3(delta_deltas)
-		scores = F.softmax(torch.cat([score1, score2, score3], dim=0), dim=0)
+		evidence3 = self.evidence2(delta_deltas)
+		score3 = self.attention_vector(evidence3)
 
+		scores = self.softmax(torch.cat[evidence1, evidence2, evidence3],dim=0))
 		context = scores[0]*poses + scores[1]*deltas + scores[2]*delta_deltas
 		context = self.classify(context)
 		out = torch.sigmoid(context)
-
 
 		return out, torch.cat([score1, score2, score3], dim=1).detach()
 

models/emotion_classification/data/torch_datasets.py
@@ -6,8 +6,9 @@ import pandas as pd
 from torch.utils.data import Dataset
 from argparse import ArgumentParser
 
-PROJECT_DIR = '/'.join(os.path.dirname(os.path.realpath(__file__)).split("/")[:-2])
+PROJECT_DIR = '/'.join(os.path.dirname(os.path.realpath(__file__)).split("/")[:-3])
 sys.path.insert(0, PROJECT_DIR)
+print(PROJECT_DIR)
 from definitions import constants
 MODELS_DIR = constants["MODELS_DIR"]
 sys.path.insert(0, MODELS_DIR)