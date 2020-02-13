ef0d6398b5eb00b0a31ad6c0be317a99417b59f5
Renou Benteau <abenteau@gmail.com>
2020-02-13 19:47:10 +0100

    progress on some cleaning

--- models/emotion_classification/Linear/run_many.sh
+++
@@ -1,4 +0,0 @@
-CUDA_VISIBLE_DEVICES=5 python3 train_linear.py -interp -emotion 0 -epochs 70 > outs/anger-interp.out &
-CUDA_VISIBLE_DEVICES=5 python3 train_linear.py -interp -emotion 1 -epochs 70 > outs/happiness-interp.out &
-CUDA_VISIBLE_DEVICES=5 python3 train_linear.py -interp -emotion 2 -epochs 70 > outs/sadness-interp.out &
-CUDA_VISIBLE_DEVICES=5 python3 train_linear.py -interp -emotion 3 -epochs 70 > outs/surprise-interp.out &
