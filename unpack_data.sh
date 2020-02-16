17e8c0233bca30060dad9618c2eb27c84c0eb5fd
Renou Benteau <renou.benteau@ims.uni-stuttgart.de>
2020-02-16 15:36:25 +0100

    add script to unpack data

---
+++ unpack_data.sh
@@ -0,0 +1,3 @@
+unzip zips/emotion_classification_data.zip -d models/emotion_classification/data
+unzip zips/pose_alignment_data.zip -d models/visual_pose_alignment/data
+unzip zips/reference_photos.zip -d MPIIEmo/annos_website/actor_ids
