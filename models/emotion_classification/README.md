29e23e0a121d695d2daec9775f479476800a9e8e
Renou Benteau <renou.benteau@ims.uni-stuttgart.de>
2020-02-18 13:32:13 +0100

    added expected toy output to emotion_classification

---
+++ models/emotion_classification/README.md
@@ -0,0 +1,7 @@
+run run_toy.sh
+then scores_to_csv.py
+then print_results.py
+
+Output will have errors, but should have results for one JointBiLSTM model. Compare this to expected_toy_output.txt
+
+If this works, run run_all.sh and repeat the same process.
--- models/emotion_classification/print_results.py
+++ models/emotion_classification/print_results.py
@@ -30,9 +30,13 @@ for model in ["rand", "SVM", "Linear", "CNN", "attCNN", "BiLSTM", "JointBiLSTM"]
         av_model_results = av_df[(av_df.emotion == emotion) & (av_df.model == model_key)
                             & (av_df.feats == feats) & ((av_df.body_part == body_part)
                             | (av_df.body_part.isna()))]
+        if len(av_model_results) == 0:
+            continue
+
         foldwise_model_results = folds_df[(folds_df.emotion == emotion) & (folds_df.model == model_key)
                             & (folds_df.feats == feats) & ((folds_df.body_part == body_part)
                             | (folds_df.body_part.isna()))]
+
 
         av_f_scores[model][emotion] = (av_model_results.f.iloc[0], av_model_results.f_std.iloc[0])
         foldwise_f_scores[model][emotion] = list(foldwise_model_results.f)
@@ -48,6 +52,8 @@ for model in ["rand", "SVM", "Linear", "CNN", "attCNN", "BiLSTM", "JointBiLSTM"]
     mean_std = 0
     all_f_scores = []
     for emotion in ["anger", "happiness", "sadness", "surprise"]:
+        if model not in av_f_scores or emotion not in av_f_scores[model]:
+            continue
         # print(av_f_scores[model])
         to_print += "{:<6.2f}/ {:<6.2f}{:3}".format(av_f_scores[model][emotion][0]*100,
                                                 av_f_scores[model][emotion][1]*100,"")
@@ -61,39 +67,38 @@ for model in ["rand", "SVM", "Linear", "CNN", "attCNN", "BiLSTM", "JointBiLSTM"]
     print(to_print)
 
 # p values
-
-print("\n\nP values between different model outputs, for all emotions and per emotion.")
-models = ["rand", "SVM","Linear","CNN","attCNN","BiLSTM","JointBiLSTM"]
-foldwise_fs_per_model = {}
-for model in models:
-    if model not in foldwise_fs_per_model:
-        foldwise_fs_per_model[model] = []
-        for emotion in ["anger", "happiness", "sadness", "surprise"]:
-            foldwise_fs_per_model[model] += foldwise_f_scores[model][emotion]
-
-
-model_p_df = {}
-for model1 in models:
-    if model1 not in model_p_df:
-        model_p_df[model1] = []
-    for model2 in models:
-        model_p_df[model1].append(ttest_ind(foldwise_fs_per_model[model1],
-                                    foldwise_fs_per_model[model2], equal_var=False)[1])
-model_p_df = (pd.DataFrame(model_p_df, index=models)*100).round(decimals=2)
-print("\nGLOBAL")
-print(model_p_df)
-emotion_p_dfs = {}
-for emotion in ["anger", "happiness", "sadness", "surprise"]:
-    emotion_p_dict = {"rand":[],"SVM":[],"Linear":[], "CNN":[], "attCNN":[],
-                        "BiLSTM":[], "JointBiLSTM":[]}
-    for model1 in ["rand", "SVM","Linear","CNN","attCNN","BiLSTM","JointBiLSTM"]:
-        for model2 in ["rand", "SVM", "Linear", "CNN","attCNN","BiLSTM","JointBiLSTM"]:
-            # if model1 == model2:
-            #     emotion_p_dict[model1].append(1)
-            # else:
-            emotion_p_dict[model1].append(ttest_ind(foldwise_f_scores[model1][emotion],
-                                                foldwise_f_scores[model2][emotion], equal_var = False)[1])
-    emotion_p_dfs[emotion] = (pd.DataFrame(emotion_p_dict,index=models)*100).round(decimals=2)
-    print("\n")
-    print(emotion.upper())
-    print(emotion_p_dfs[emotion])
+#
+# print("\n\nP values between different model outputs, for all emotions and per emotion.")
+# models = ["rand", "SVM","Linear","CNN","attCNN","BiLSTM","JointBiLSTM"]
+# foldwise_fs_per_model = {}
+# for model in models:
+#     if model not in foldwise_fs_per_model:
+#         foldwise_fs_per_model[model] = []
+#         for emotion in ["anger", "happiness", "sadness", "surprise"]:
+#             if model not in foldwise_f_scores or emotion not in foldwise_f_scores[model]:
+#                 continue
+#             foldwise_fs_per_model[model] += foldwise_f_scores[model][emotion]
+#
+#
+# model_p_df = {}
+# for model1 in models:
+#     if model1 not in model_p_df:
+#         model_p_df[model1] = []
+#     for model2 in models:
+#         model_p_df[model1].append(ttest_ind(foldwise_fs_per_model[model1],
+#                                     foldwise_fs_per_model[model2], equal_var=False)[1])
+# model_p_df = (pd.DataFrame(model_p_df, index=models)*100).round(decimals=2)
+# print("\nGLOBAL")
+# print(model_p_df)
+# emotion_p_dfs = {}
+# for emotion in ["anger", "happiness", "sadness", "surprise"]:
+#     emotion_p_dict = {"rand":[],"SVM":[],"Linear":[], "CNN":[], "attCNN":[],
+#                         "BiLSTM":[], "JointBiLSTM":[]}
+#     for model1 in ["rand", "SVM","Linear","CNN","attCNN","BiLSTM","JointBiLSTM"]:
+#         for model2 in ["rand", "SVM", "Linear", "CNN","attCNN","BiLSTM","JointBiLSTM"]:
+#             emotion_p_dict[model1].append(ttest_ind(foldwise_f_scores[model1][emotion],
+#                                                 foldwise_f_scores[model2][emotion], equal_var = False)[1])
+#     emotion_p_dfs[emotion] = (pd.DataFrame(emotion_p_dict,index=models)*100).round(decimals=2)
+#     print("\n")
+#     print(emotion.upper())
+#     print(emotion_p_dfs[emotion])
