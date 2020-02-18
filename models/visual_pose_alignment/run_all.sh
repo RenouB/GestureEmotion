dbe90c34555281ba01206c6c3d22aa891bad8f84
Renou Benteau <renou.benteau@ims.uni-stuttgart.de>
2020-02-18 14:09:55 +0100

    change vispos expected output

--- models/visual_pose_alignment/run_all.sh
+++ models/visual_pose_alignment/run_all.sh
@@ -9,5 +9,13 @@ python3 train_svm_voting.py
 cd ../whole_image
 python3 train_svm_whole_image.py
 cd ../..
-more random/outputs/scores/*csv | cat
-more */*/*/scores/*csv | cat
+echo "RANDOM"
+cat random/outputs/scores/*csv
+echo "DISTANCE WHOLE"
+cat distance/whole_image/outputs/scores/*csv
+echo "DISTANCE VOTE"
+cat distance/voting/outputs/scores/*csv
+echo "SVM WHOLE IMAGE"
+cat SVM/whole_image/outputs/scores/*csv
+echo "SVM VOTE"
+cat SVM/voting/outputs/scores/*csv
