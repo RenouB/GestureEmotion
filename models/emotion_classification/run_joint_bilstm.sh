41b1ec9adb1f8eb5ea1d786327cc54235f774c03
Renou Benteau-gpu1 <renoub@gpu-1.hcics.simtech.uni-stuttgart.de>
2020-02-20 22:24:30 +0000

    fix typo in run bilstm

--- models/emotion_classification/run_joint_bilstm.sh
+++ models/emotion_classification/run_joint_bilstm.sh
@@ -1,6 +1,6 @@
 find JointBiLSTM -name *pkl -exec rm {} \;
 cd JointBiLSTM
-CUDA_VISIBLE_DEVICES=3 python3 train_joint_bilstm.py -emotion 0 -cuda -num_folds 8 -epochs 100 -interp -keypoints full -input brute -debug &
-CUDA_VISIBLE_DEVICES=3 python3 train_joint_bilstm.py -emotion 1 -cuda -num_folds 8 -epochs 100 -interp -keypoints full -input brute -debug &
-CUDA_VISIBLE_DEVICES=3 python3 train_joint_bilstm.py -emotion 2 -cuda -num_folds 8 -epochs 100 -interp -keypoints full -input brute -debug &
-CUDA_VISIBLE_DEVICES=3 python3 train_joint_bilstm.py -emotion 3 -cuda -num_folds 8 -epochs 100 -interp -keypoints full -input brute -debug &
+CUDA_VISIBLE_DEVICES=3 python3 train_joint_bilstm.py -emotion 0 -cuda -num_folds 8 -epochs 100 -interp -keypoints full -input brute  &
+CUDA_VISIBLE_DEVICES=3 python3 train_joint_bilstm.py -emotion 1 -cuda -num_folds 8 -epochs 100 -interp -keypoints full -input brute  &
+CUDA_VISIBLE_DEVICES=3 python3 train_joint_bilstm.py -emotion 2 -cuda -num_folds 8 -epochs 100 -interp -keypoints full -input brute  &
+CUDA_VISIBLE_DEVICES=3 python3 train_joint_bilstm.py -emotion 3 -cuda -num_folds 8 -epochs 100 -interp -keypoints full -input brute  &
